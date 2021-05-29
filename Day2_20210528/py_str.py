77b96d6f1e37dcb07c4f9ee8f1dcd929c5f0cb2b
twcxjq <tianchangjun_nsu@163.com>
2021-05-29 17:55:50 +0800

    python_29第四次提交

--- Day2_20210528/py_str.py
+++ Day2_20210528/py_str.py
@@ -4,7 +4,7 @@
 # FileName:     py_str
 # Author:      TianChangJun
 # Datetime:    2021/5/28 16:44
-# Description：
+# Description：字符串是Python 中最常用的数据类型。我们可以使用引号( ' 或 " )来创建字符串
 #-----------------------------------------------------------------------------------
 
 # 创建一个空字符串
@@ -14,3 +14,10 @@ str2 = "";
 # 查看字符串长度 len(对象)
 print(type(str1),len(str1));
 print(type(str2),len(str2));
+
+# 定义一个非空字符串
+str3 = "hello_python";
+print(type(str3), len(str3));
+
+
+
